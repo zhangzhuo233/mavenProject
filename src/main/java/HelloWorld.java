36a13f1cc0cd5aaa80cc2dc92d21364fcf8a6f12
zhangzhuo233 <zhangzhuo2018@yeah.net>
2019-07-02 23:52:59 +0800

    init maven project

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class HelloWorld {
+    public String sayHello() {
+        return "hello world";
+    }
 }
--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class HelloWorldTest {
+    @Test
+    public void sayHello() {
+        Assert.assertEquals("hello world", new HelloWorld().sayHello());
+    }
 }
